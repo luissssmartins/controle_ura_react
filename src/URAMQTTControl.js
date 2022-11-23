02d918cfcd1a743172cd4efaa234cc17627414de
Luis Martins <chateadorn@gmail.com>
2022-11-23 12:21:24 -0300

    mudando algumas coisas em relação a autenticação do WS

--- src/URAMQTTControl.js
+++ src/URAMQTTControl.js
@@ -9,7 +9,11 @@ export default function URAMQTTControl(props) {
   console.log("props " + props.outputTopic)
   return (
     <div>
+<<<<<<< Updated upstream
       <Connector brokerUrl="ws://10.6.3.134:1884" options={{ keepalive: 0 }} >
+=======
+      <Connector brokerUrl="ws://mqtt:lar_mqtt@10.6.1.42:1884" options={{ keepalive: 1 }} >
+>>>>>>> Stashed changes
         <BasicControl topic={props.inputTopic} />
         <RobotStatus topic={props.outputTopic} />
       </Connector>
